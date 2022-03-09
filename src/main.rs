801e085a1eddfc1ecab7c0f44f3f117e1ff27e9a
Obsido Han <maghsk2017@gmail.com>
2022-03-09 16:53:19 +0800

    refactor + clap arg parser

--- src/main.rs
+++ src/main.rs
@@ -2,7 +2,6 @@ extern crate base64;
 extern crate byteorder;
 extern crate crypto;
 extern crate id3;
-#[macro_use]
 extern crate json;
 extern crate metaflac;
 extern crate tempfile;
@@ -11,14 +10,25 @@ use byteorder::ByteOrder;
 use byteorder::NativeEndian;
 use crypto::buffer::{BufferResult, ReadBuffer, WriteBuffer};
 use crypto::{aes, blockmodes, buffer, symmetriccipher};
+use json::JsonValue;
 use std::io::prelude::*;
 use std::io::SeekFrom;
-use std::{env, mem};
+use std::mem;
 use tempfile::NamedTempFile;
 
 const AES_CORE_KEY: &[u8; 16] = b"\x68\x7A\x48\x52\x41\x6D\x73\x6F\x35\x6B\x49\x6E\x62\x61\x78\x57";
 const AES_MODIFY_KEY: &[u8; 16] =
     b"\x23\x31\x34\x6C\x6A\x6B\x5F\x21\x5C\x5D\x26\x30\x55\x3C\x27\x28";
+const FILTER: [(&str, &str); 8] = [
+    ("\\", "＼"),
+    ("/", "／"),
+    (":", "："),
+    ("*", "＊"),
+    ("\"", "＂"),
+    ("<", "＜"),
+    (">", "＞"),
+    ("|", "｜"),
+];
 
 fn build_key_box(key: &[u8]) -> [u8; 256] {
     let key_len = key.len();
@@ -74,19 +84,14 @@ fn decrypt(
 }
 
 fn process_file(path: &std::path::Path) -> std::io::Result<()> {
-    let mut ulen: u32;
-    // let i: i32;
-
     let mut f = std::fs::File::open(path).expect("cannot open source file:");
     let mut buf = [0u8; mem::size_of::<u32>()];
     f.read(&mut buf)?;
-    ulen = NativeEndian::read_u32(&buf);
-    if ulen != 0x4e455443 {
+    if NativeEndian::read_u32(&buf) != 0x4e455443 {
         panic!("Not a netease music file.")
     }
     f.read(&mut buf)?;
-    ulen = NativeEndian::read_u32(&buf);
-    if ulen != 0x4d414446 {
+    if NativeEndian::read_u32(&buf) != 0x4d414446 {
         panic!("Not a netease music file.")
     }
     f.seek(SeekFrom::Current(2))?;
@@ -96,9 +101,7 @@ fn process_file(path: &std::path::Path) -> std::io::Result<()> {
     let mut key_data: Vec<u8> = Vec::with_capacity(key_len as usize);
     key_data.resize(key_len as usize, 0);
     f.read_exact(&mut key_data)?;
-    for i in 0..key_len {
-        (&mut key_data)[i as usize] ^= 0x64;
-    }
+    key_data.iter_mut().for_each(|x| *x ^= 0x64);
 
     //    let cipher = Cipher::aes_128_ecb();
     let de_key_data = decrypt(&key_data, AES_CORE_KEY).expect("error decrypting key data:");
@@ -106,17 +109,12 @@ fn process_file(path: &std::path::Path) -> std::io::Result<()> {
     //    let de_key_len = de_key_data.len() as u32;
     let kbox = build_key_box(&de_key_data[17..]);
     f.read(&mut buf)?;
-    ulen = NativeEndian::read_u32(&buf);
-    let mut has_metadata = false;
-    let mut music_info = object! {};
-    if ulen > 0 {
-        has_metadata = true;
+    let ulen = NativeEndian::read_u32(&buf);
+    let music_info = if ulen > 0 {
         let mut modify_data: Vec<u8> = Vec::with_capacity(ulen as usize);
         modify_data.resize(ulen as usize, 0);
         f.read_exact(&mut modify_data)?;
-        for i in 0..ulen {
-            modify_data.as_mut_slice()[i as usize] ^= 0x63;
-        }
+        modify_data.iter_mut().for_each(|x| *x ^= 0x63);
         // let data_len: usize;
         // let mut data: Vec<u8> = Vec::with_capacity(ulen as usize);
         // data.resize(ulen as usize, 0);
@@ -127,29 +125,23 @@ fn process_file(path: &std::path::Path) -> std::io::Result<()> {
         //    let dedata = decrypt(cipher, AES_MODIFY_KEY, None, &data).expect("error decrypting data:");
         let dedata = decrypt(&data, AES_MODIFY_KEY).expect("error decrypting data:");
 
-        music_info =
+        Some(
             json::parse(std::str::from_utf8(&dedata[6..]).expect("music info is not valid utf-8:"))
-                .expect("error parsing json:");
+                .expect("error parsing json:"),
+        )
     } else {
         println!(
             "{} has no metadata.",
             path.file_name().unwrap().to_str().unwrap()
         );
-    }
+        None
+    };
     let s = path.file_name().unwrap().to_str().unwrap();
-    let mut music_filename = s.get(0..s.len() - 4).unwrap().to_owned();
-    let mut filter = std::collections::HashMap::new();
-    filter.insert("\\", "＼");
-    filter.insert("/", "／");
-    filter.insert(":", "：");
-    filter.insert("*", "＊");
-    filter.insert("\"", "＂");
-    filter.insert("<", "＜");
-    filter.insert(">", "＞");
-    filter.insert("|", "｜");
-    for (k, v) in filter.iter() {
-        music_filename = music_filename.replace(k, v);
-    }
+
+    let music_filename = FILTER.iter().fold(
+        s.get(0..s.len() - 4).unwrap().to_owned(),
+        |acc, (fr, to)| acc.replace(fr, to),
+    );
 
     println!("{}", music_filename);
 
@@ -158,42 +150,43 @@ fn process_file(path: &std::path::Path) -> std::io::Result<()> {
     f.seek(SeekFrom::Current(9))?;
     f.read(&mut buf)?;
     let img_len: u32 = NativeEndian::read_u32(&buf);
-    let mut _has_cover = false;
-    let mut img_data: Vec<u8> = vec![0];
-    if img_len > 0 {
-        _has_cover = true;
-        img_data = Vec::with_capacity(img_len as usize);
-        img_data.resize(img_len as usize, 0);
-        f.read_exact(&mut img_data)?;
+    let img_data = if img_len > 0 {
+        let mut t = Vec::with_capacity(img_len as usize);
+        t.resize(img_len as usize, 0);
+        f.read_exact(&mut t)?;
+        Some(t)
     } else {
         println!(
             "{} has no cover image.",
             path.file_name().unwrap().to_str().unwrap()
         );
-    }
-    let mut n: usize = 0x8000;
+        None
+    };
     let mut buffer = [0u8; 0x8000];
     let mut tmp = NamedTempFile::new()?;
     let mut format = "undefined";
-    let mut filter_music_filename = music_filename;
-    while n > 1 {
-        n = f.read(&mut buffer)?;
-        for i in 0..n {
-            let j = (i + 1) & 0xff;
-            // box[(box[j] + box[(box[j] + j) & 0xff]) & 0xff];
-            buffer[i] ^=
-                kbox[(kbox[j] as usize + kbox[(kbox[j] as usize + j) & 0xff] as usize) & 0xff];
+    loop {
+        let n = f.read(&mut buffer)?;
+        if n == 0 {
+            break;
         }
-        if format == "undefined" {
-            if buffer[0] == 0x49 && buffer[1] == 0x44 && buffer[2] == 0x33 {
-                format = "mp3";
-            } else {
-                format = "flac"
-            }
-            filter_music_filename = filter_music_filename + "." + format;
+        for (i, x) in buffer.iter_mut().enumerate() {
+            let j = (i + 1) & 0xff;
+            *x ^= kbox[(kbox[j] as usize + kbox[(kbox[j] as usize + j) & 0xff] as usize) & 0xff];
         }
         tmp.write(&buffer)?;
+        if n > 2 {
+            if format == "undefined" {
+                format = if buffer[0..2] == [0x49, 0x44, 0x33] {
+                    "mp3"
+                } else {
+                    "flac"
+                };
+            }
+        }
     }
+    let format = format;
+    let filter_music_filename = music_filename + "." + format;
     // let mut fmusic = std::fs::File::create(std::path::Path::new(&filter_music_filename))?;
     std::fs::copy(
         tmp.into_temp_path(),
@@ -201,33 +194,35 @@ fn process_file(path: &std::path::Path) -> std::io::Result<()> {
     )?;
     // drop(tmp);
     drop(f);
-    if has_metadata {
-        let mut mimetype = "";
-        if _has_cover {
-            let png: Vec<u8> = vec![0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A];
-            if png == &img_data[..8] {
-                mimetype = "image/png";
+    if let Some(music_info) = music_info {
+        let mimetype = if let Some(ref img_data) = img_data {
+            if img_data[..8] == [0x89, 0x50, 0x4E, 0x47, 0x0D, 0x0A, 0x1A, 0x0A] {
+                "image/png"
             } else {
-                mimetype = "image/jpeg";
+                "image/jpeg"
             }
-        }
+        } else {
+            ""
+        };
         let music_name = music_info["musicName"].as_str().unwrap();
         let album = music_info["album"].as_str().unwrap();
         let artist = &music_info["artist"];
         let _bitrate = music_info["bitrate"].as_i64().unwrap();
         let _duration = music_info["duration"].as_i64().unwrap();
+        let artists: Vec<&str> = if let JsonValue::Array(arr) = artist {
+            arr.iter()
+                .map(|x| x[0].as_str().unwrap_or_default())
+                .collect()
+        } else {
+            vec![]
+        };
         if format == "mp3" {
             let mut tag = id3::Tag::read_from_path(std::path::Path::new(&filter_music_filename))
                 .unwrap_or(id3::Tag::new());
             tag.set_title(music_name);
             tag.set_album(album);
-            let mut artists = String::from(artist[0][0].as_str().unwrap());
-            for i in 1..artist.len() {
-                artists += "/";
-                artists += artist[i][0].as_str().unwrap();
-            }
-            tag.set_artist(artists);
-            if _has_cover {
+            tag.set_artist(artists.join("/"));
+            if let Some(img_data) = img_data {
                 let picture = id3::frame::Picture {
                     mime_type: mimetype.to_owned(),
                     picture_type: id3::frame::PictureType::CoverFront,
@@ -249,12 +244,8 @@ fn process_file(path: &std::path::Path) -> std::io::Result<()> {
             let c = tag.vorbis_comments_mut();
             c.set_title(vec![music_name]);
             c.set_album(vec![album]);
-            let mut artists: Vec<String> = Vec::new();
-            for i in 0..artist.len() {
-                artists.push(artist[i][0].as_str().unwrap().to_string());
-            }
             c.set_artist(artists);
-            if _has_cover {
+            if let Some(img_data) = img_data {
                 tag.add_picture(mimetype, metaflac::block::PictureType::CoverFront, img_data);
             }
             tag.write_to_path(std::path::Path::new(&filter_music_filename))
@@ -264,7 +255,11 @@ fn process_file(path: &std::path::Path) -> std::io::Result<()> {
     Ok(())
 }
 fn main() {
-    assert!(env::args().len() >= 2);
-    let args: Vec<String> = env::args().collect();
-    process_file(std::path::Path::new(&args[1])).expect("process error at main:");
+    let arg = clap::Arg::new("input_file").index(1).required(true);
+    let cmd = clap::Command::new("ncmdump").bin_name("ncmdump").arg(arg);
+    let matches = cmd.get_matches();
+    process_file(std::path::Path::new(
+        matches.value_of("input_file").unwrap(),
+    ))
+    .expect("process error at main:");
 }
